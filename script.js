18faaadc20edc9f9a9df0cf9a2261eb98ec529fa
Roman <kavoriya@gmail.com>
2022-12-18 16:36:26 +0500

    Create a button to paint squares with random colors
    Buttons with grid sizes work.
    Button to remove grid works.
    Create a button, which will change color of squares to rainbdow
    colors when hovering over squares. As weel as a button which will
    change color to black.

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 let container = document.getElementById("container");
 
-createGrid(256);
+let btnRemoveGrid = document.getElementById("removeGrid");
+btnRemoveGrid.addEventListener("click", removeGrid);
 
-function createGrid(numberOfSquares) {
+let btn16x16 = document.getElementById("btn16x16");
+btn16x16.addEventListener("click", () => {
+  removeGrid()
+  createGrid(16, 16);
+});
+
+let btn24x24 = document.getElementById("btn24x24");
+btn24x24.addEventListener("click", () => {
+  removeGrid()
+  createGrid(24, 24);
+});
+
+let btn40x40 = document.getElementById("btn40x40");
+btn40x40.addEventListener("click", () => {
+  removeGrid()
+  createGrid(40, 40);
+});
+
+let btn96x96 = document.getElementById("btn96x96");
+btn96x96.addEventListener("click", () => {
+  removeGrid()
+  createGrid(96, 96);
+});
+
+function createGrid(xAxis, yAxis) {
+  container.style.gridTemplateColumns = `repeat(${xAxis}, ${960/xAxis}px)`;
+  container.style.gridTemplateRows = `repeat(${yAxis}, ${960/yAxis}px)`;
   container.style.borderLeft = "1px solid black";
   container.style.borderTop = "1px solid black";
-  for (let i = 1; i <= numberOfSquares; i++) {
+  for (let i = 1; i <= (xAxis * yAxis); i++) {
     let newSquare = document.createElement("div");
     newSquare.style.borderRight = "1px solid black";
     newSquare.style.borderBottom = "1px solid black";
@@ -16,4 +43,11 @@ function createGrid(numberOfSquares) {
     );
     container.appendChild(newSquare);
   }
+}
+
+function removeGrid() {
+  container.style.borderLeft = "none";
+  container.style.borderTop = "none";
+  let squares = document.querySelectorAll(".square");
+  squares.forEach(square => square.remove());
 }
