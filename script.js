bf150799e519a988ee4eb440012093b9343967a9
Roman <kavoriya@gmail.com>
2022-12-18 14:39:09 +0500

    Make sqaures change colors
    Function createGrid() works with 256 squares.
    Make squares change colors when being hovered over.
    Create buttons with preset number of squares.

---
+++ script.js
@@ -0,0 +1,15 @@
+let container = document.getElementById("container");
+
+createGrid(256);
+
+function createGrid(numberOfSquares) {
+  container.style.borderLeft = "1px solid black";
+  container.style.borderTop = "1px solid black";
+  for (let i = 1; i <= numberOfSquares; i++) {
+    let newSquare = document.createElement("div");
+    newSquare.style.borderRight = "1px solid black";
+    newSquare.style.borderBottom = "1px solid black";
+    newSquare.style.boxSizing = "border-box";
+    container.appendChild(newSquare);
+  }
+}
